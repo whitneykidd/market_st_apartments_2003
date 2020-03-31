43e1e34fa36847c5edacc728961faa4d3414797d
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-31 09:15:09 -0600

    Pass test_it_exists

---
+++ test/building_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter.rb'
+require './lib/apartment.rb'
+require './lib/building.rb'
+
+class BuildingTest < Minitest::Test
+  def setup
+    @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+  end
+
+  def test_it_exists
+    assert_instance_of Building, @building
+  end
+end
